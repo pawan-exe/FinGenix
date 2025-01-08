3d4a43362c000c0d22a51f3c083e6cbe2946eea7
pawan-exe <pawangupta63431@gmail.com>
2025-01-08 16:49:30 +0530

    generate monthly reports functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,19 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/function";
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [checkBudgetAlerts],
+  functions: [
+    checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+    generateMonthlyReports,
+  ],
 });
